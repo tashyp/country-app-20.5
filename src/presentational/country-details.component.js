ccee4e920401cb39d2a00e6f7e8c0c61f8ce3036
Przemek Saja <przemek.saja@gmail.com>
2018-09-05 16:16:42 +0200

    Make change with img - alt is already''

--- src/presentational/country-details.component.js
+++ src/presentational/country-details.component.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CountryDetails = (props) => (
     <div className="country-wrapper">
         <header>
-            <img className="country-photo" src={props.country.imageUrl} alt="country photo" />
+            <img className="country-photo" src={props.country.imageUrl} alt='' />
         </header>
         <div className="country-info">
             <h1>{props.country.name}</h1>
